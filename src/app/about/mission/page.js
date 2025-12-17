f13f6514bb074116945571e8546c732c879e5c4b
Linda <89836796+lindashao1220@users.noreply.github.com>
2025-12-17 15:46:26 +0100

    ww

--- src/app/about/mission/page.js
+++ src/app/about/mission/page.js
@@ -12,7 +12,7 @@ export default function Mission() {
                     <Image src="/logo_white.png" alt="Logo" width={50} height={50} />
                 </div>
                 <nav className="flex space-x-4">
-                    <a href="/" className="hover:text-gray-400">Home</a>
+                    <a href="/" className="hover:text-gray-400">Home家</a>
                     {/* About Dropdown */}
                     <div className="relative group">
                         <a href="#" className="flex items-center space-x-1 hover:text-gray-400">
