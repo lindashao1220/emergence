990052c2844c0a595547a0ce66998ca758210715
Linda <89836796+lindashao1220@users.noreply.github.com>
2025-12-19 11:12:30 +0100

    ee

--- src/app/projects/newmedia4/page.js
+++ src/app/projects/newmedia4/page.js
@@ -8,36 +8,6 @@ import UnseenCarousel from "@/components/nm1carousel";
 export default function NewMedia4() {
     return (
         <div className="bg-black text-white min-h-screen flex flex-col">
-            {/* Fixed Header */}
-            <header className="fixed top-0 left-0 w-full flex justify-between items-center p-6 bg-black z-10">
-                <div className="text-2xl font-bold">
-                    <Image src="/logo_white.png" alt="Logo" width={50} height={50}/>
-                </div>
-                <nav className="flex space-x-4">
-                    <a href="/" className="hover:text-gray-400">Home</a>
-                    {/* About Dropdown */}
-                    <div className="relative group">
-                        <a href="#" className="flex items-center space-x-1 hover:text-gray-400">
-                            <span>About</span>
-                            <span className="text-sm">&#9662;</span> {/* Down arrow */}
-                        </a>
-                        {/* Dropdown Menu */}
-                        <div
-                            className="absolute left-0 top-full mt-2 w-40 bg-black border border-gray-600 rounded-md opacity-0 group-hover:opacity-100 transition-opacity">
-                            <a href="/about/team"
-                               className="block px-4 py-2 hover:bg-gray-800 border-b border-gray-600">
-                                Team
-                            </a>
-                            <a href="/about/mission" className="block px-4 py-2 hover:bg-gray-800">
-                                Mission
-                            </a>
-                        </div>
-                    </div>
-                    <a href="/contact" className="hover:text-gray-400">Contact</a>
-                    <a href="/projects" className="bg-yellow-400 text-black px-4 py-2 rounded-full">Projects</a>
-                    <a href="#" className="bg-pink-600 text-white px-4 py-2 rounded-full">PKB</a>
-                </nav>
-            </header>
 
             {/* Main Content - video */}
             <main className="pt-[80px]">
